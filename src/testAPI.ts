71ed8bb7b44bc9b0b5b19b191454563a6e22fb1e
CharlesMulloy <CharlesMulloy@users.noreply.github.com>
2018-09-03 13:14:16 -0400

    Tweaked tests to make sure featreu from last commit works.

--- src/testAPI.ts
+++ src/testAPI.ts
@@ -7,39 +7,39 @@ describe("EpubBuilder", function() {
       assert.notEqual(ebook === null);
     })
 
-    it("Should allow the setting of the books' properies", function() {
-      ebook.title = "This is a title";
-      ebook.author = "This is the author";
-      ebook.summary = "This is the summary";
+    // it("Should allow the setting of the books' properies", function() {
+    //   ebook.title = "This is a title";
+    //   ebook.author = "This is the author";
+    //   ebook.summary = "This is the summary";
 
-      assert.ok(ebook.getTitle() == "This is a title");
-      assert.ok(ebook.getAuthor() == "This is the author");
-      assert.ok(ebook.getSummary() == "This is the summary");
-    });
+    //   assert.ok(ebook.getTitle() == "This is a title");
+    //   assert.ok(ebook.getAuthor() == "This is the author");
+    //   assert.ok(ebook.getSummary() == "This is the summary");
+    // });
 
-    it("Should allow the creation of chapters", function() {
-      assert.equal(ebook.getChapter().length, 0);
+    // it("Should allow the creation of chapters", function() {
+    //   assert.equal(ebook.getChapter().length, 0);
 
-      ebook.addChapter("Chapter 1", "<p>The story begins...</p>");
+    //   ebook.addChapter("Chapter 1", "<p>The story begins...</p>");
 
-      assert.equal(ebook.getChapter().length, 1);
-      assert.equal(ebook.getChapter(0).title, "Chapter 1");
-      assert.equal(ebook.getChapter(0).content, "<p>The story begins...</p>");
-    });
+    //   assert.equal(ebook.getChapter().length, 1);
+    //   assert.equal(ebook.getChapter(0).title, "Chapter 1");
+    //   assert.equal(ebook.getChapter(0).content, "<p>The story begins...</p>");
+    // });
 
-    it("Should allow the addition of assets", function() {
-      ebook.addChapter("Chapter 1", "<p><img src=\"image.jpeg\" alt=\"Test image\"/></p>");
-      ebook.addAsset("./src/testAssets/image.jpeg");
-      ebook.createBook("testbook");
-    });
-
-
-    // it("Should allow the addition of assets in other folders", function() {
-    //   ebook.title = "Title"
-    //   ebook.addChapter("Chapter 1", `<p><img src="images/image.jpeg" alt="Test image"/></p>`);
-    //   ebook.addAsset("./src/testAssets/image.jpeg", "images/image.jpeg");
+    // it("Should allow the addition of assets", function() {
+    //   ebook.addChapter("Chapter 1", "<p><img src=\"image.jpeg\" alt=\"Test image\"/></p>");
+    //   ebook.addAsset("./src/testAssets/image.jpeg");
     //   ebook.createBook("testbook");
     // });
+
+
+    it("Should allow the addition of assets in other folders", function() {
+      ebook.title = "Title"
+      ebook.addChapter("Chapter 1", `<p><img src="images/image.jpeg" alt="Test image"/></p>`);
+      ebook.addAsset("./src/testAssets/image.jpeg", "images/image.jpeg");
+      ebook.createBook("testbook");
+    });
 
   });
 });
